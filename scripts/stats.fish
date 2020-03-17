238303195c3068ffb19177b6cb40c3b66ba77953
Mathias Iversen <mathias.iversen@advisa.se>
2020-03-17 11:46:00 +0100

    fixed script on mac

--- scripts/stats.fish
+++ scripts/stats.fish
@@ -1,9 +1,9 @@
-#!/usr/bin/fish
+#!/usr/local/bin/fish
 
-set stats_file 'stats.json'
+set stats_file "stats.json"
 
 set mode $argv[1]
 
 node node_modules/webpack/bin/webpack.js --profile --json > $stats_file --config webpack/$mode.config.js;
-sed -i '1,3d' $stats_file;
+sed -i "" "1,3d" $stats_file;
 node_modules/webpack-bundle-analyzer/lib/bin/analyzer.js --default-sizes gzip $stats_file ./dist;
